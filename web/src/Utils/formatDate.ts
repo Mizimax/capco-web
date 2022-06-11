725a13604272b9a32e1f53aa199cf862347db66e
tammanoonjomjaturong <max.tammanoon@skooldio.com>
2022-06-11 19:20:27 +0700

    🐛  Handle invalid date string

--- web/src/Utils/formatDate.ts
+++ web/src/Utils/formatDate.ts
@@ -1,14 +1,19 @@
 export const getFullDateString = (dateString: string) => {
-  const date = new Date(dateString);
-  // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
-  const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    hourCycle: 'h24',
-    minute: 'numeric',
-    timeZone: 'Asia/Bangkok',
-  };
-  return new Intl.DateTimeFormat('en-US', options).format(date);
+  try {
+    const date = new Date(dateString);
+    // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      hourCycle: 'h24',
+      minute: 'numeric',
+      timeZone: 'Asia/Bangkok',
+    };
+    return new Intl.DateTimeFormat('en-US', options).format(date);
+  } catch (error) {
+    console.error('Date format error: ', error);
+    return '';
+  }
 };
