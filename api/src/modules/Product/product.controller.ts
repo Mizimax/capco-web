52890c17379db9084db057cf95857b447e7f8751
tammanoonjomjaturong <max.tammanoon@skooldio.com>
2022-06-12 13:48:28 +0700

    search & filter product

--- api/src/modules/Product/product.controller.ts
+++ api/src/modules/Product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { ProductService } from './product.service';
 
@@ -6,8 +6,16 @@ import { ProductService } from './product.service';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
   @Get('/products')
-  getPaymentMoethod(@Res() res: Response): any {
-    const productListResult = this.productService.getProductList();
+  getPaymentMoethod(
+    @Query('search') search: string,
+    @Query('filter') filter: string,
+    @Res() res: Response,
+  ): any {
+    const filterList = filter ? filter.split(',') : undefined;
+    const productListResult = this.productService.getProductList({
+      search,
+      filter: filterList,
+    });
     res.status(HttpStatus.OK).json(productListResult);
   }
 }
--- api/src/modules/Product/product.service.ts
+++ api/src/modules/Product/product.service.ts
@@ -3,9 +3,27 @@ import { Injectable } from '@nestjs/common';
 import * as MOCKED_PRODUCT_LIST from './datasources/product-mock-up.json';
 import { Product } from './models/Product';
 
+interface ProductListInterface {
+  search: string;
+  filter: string[] | string;
+}
+
 @Injectable()
 export class ProductService {
-  getProductList(): Product[] {
-    return MOCKED_PRODUCT_LIST;
+  getProductList({ search, filter }: ProductListInterface): Product[] {
+    const mockedData = MOCKED_PRODUCT_LIST as Product[];
+    if (search || filter) {
+      return mockedData.filter((product) => {
+        return (
+          (search
+            ? product.productTitle
+                .toLowerCase()
+                .includes(search.toLowerCase()) ||
+              product.productDetail.toLowerCase().includes(search.toLowerCase())
+            : true) && (filter ? filter.includes(product.productType) : true)
+        );
+      });
+    }
+    return mockedData;
   }
 }
